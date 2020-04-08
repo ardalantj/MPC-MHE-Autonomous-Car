9644a991e06093d3ece4828c58034d74d9ef250a
Ardalan <ardalantj1107@gmail.com>
2020-04-08 13:44:33 -0400

    Added error function to MPC.m

--- MPC_tracking/MPC.m
+++ MPC_tracking/MPC.m
@@ -10,10 +10,40 @@ id_x = 1;
 id_y = 2;
 id_yaw = 3;
 id_vel = 4;
-id_curve = 5;
+id_path = 5;
 id_time = 6;
 
-% --- convex optimization ---
+DIM_STATE = 4;
+DIM_OUTPUT = 3;
+DIM_INPUT = 1;
+
+% Find nearest point from the state along the reference trajectory
+distance = vecnorm(ref(:,id_x:id_y)' - state(id_x:id_y)');
+[~,min_index] = min(distance);
+ref_start = ref(min_index, :);
+v_ref = ref_start(id_vel);
+
+% (debug) calculate latitude error through transformation matrix
+start_yaw = ref_start(id_yaw);
+trans_xy2lat = [cos(start_yaw), sin(start_yaw);
+             -sin(start_yaw), cos(start_yaw)];
+error_xy = (state(id_x:id_y) - ref_start(id_x:id_y))';
+error_lat = trans_xy2lat * error_xy;
+error_lat = error_lat(2);
+
+% Allocate dynamic matrices 
+A = zeros(DIM_STATE*mpc_N, DIM_STATE);
+B = zeros(DIM_STATE*mpc_N, DIM_INPUT*mpc_N);
+W = zeros(DIM_STATE*mpc_N, 1);
+C = zeros(DIM_OUTPUT*mpc_N, DIM_STATE*mpc_N);
+Q = zeros(DIM_OUTPUT*mpc_N, DIM_OUTPUT*mpc_N);
+R = zeros(DIM_INPUT*mpc_N, DIM_INPUT*mpc_N);
+
+mpc_ref_v = zeros(length(mpc_N), 1);
+ref_vec = zeros(mpc_N,5);
+
+
+%   Formulate optimization 
 %   minimize for x, s.t.
 %   J(x) = 1/2 * x' * H * x + f' * x, 
 %   A*x <= b,   lb <= x <= ub
@@ -25,6 +55,8 @@ f = a2;
 A = [];
 b = [];
 
+lb_ = -param.mpc_cons_steer_deg * deg2rad * ones(mpc_n * DIM_INPUT,1);
+ub_ = param.mpc_cons_steer_deg * deg2rad * ones(mpc_n * DIM_INPUT,1);
 options = optimoptions('quadprog','Algorithm','interior-point-convex', 'Display', 'off');
 [x, fval, exitflag, output, lambda] = quadprog(H,f,A,b,[],[],lb,ub,[],options);
 
